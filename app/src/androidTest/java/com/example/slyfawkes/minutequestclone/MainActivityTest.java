74eea3cd0b1b190fcf49c757d9418bbf9ca9c7ee
Dean Welch <deanwelch286@gmail.com>
2015-09-17 23:37:20 +0100

    added a test for the textview

--- app/src/androidTest/java/com/example/slyfawkes/minutequestclone/MainActivityTest.java
+++ app/src/androidTest/java/com/example/slyfawkes/minutequestclone/MainActivityTest.java
@@ -1,10 +1,12 @@
 package com.example.slyfawkes.minutequestclone;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
     private MainActivity mainActivityTest;
+    private TextView firstTextView;
 
     public MainActivityTest() {
         super(MainActivity.class);
@@ -14,9 +16,18 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     protected void setUp() throws Exception {
         super.setUp();
         mainActivityTest = getActivity();
+        firstTextView = (TextView) mainActivityTest.findViewById(R.id.first_text_view);
     }
 
     public void testPreconditions() {
         assertNotNull(mainActivityTest);
+        assertNotNull(firstTextView);
+    }
+
+    public void testMyFirstTestTextView_labelText() {
+        final String expected =
+                mainActivityTest.getString(R.string.hello_world);
+        final String actual = firstTextView.getText().toString();
+        assertEquals(expected, actual);
     }
 }
