b7625db70a0cc48a24b6a8568f23ce7207d7aade
Dean Welch <deanwelch286@gmail.com>
2015-09-23 22:30:14 +0100

    New game now opens the game activity

--- app/src/main/java/com/example/slyfawkes/minutequestclone/MainActivity.java
+++ app/src/main/java/com/example/slyfawkes/minutequestclone/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.slyfawkes.minutequestclone;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
@@ -10,5 +15,21 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView optionsListView = (ListView)findViewById(R.id.listview);
+
+        optionsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String selection = ((TextView) view).getText().toString();
+
+                switch (selection) {
+                    case "New Game":
+                        Intent i = new Intent(getApplicationContext(), GameActivity.class);
+                        startActivity(i);
+                }
+            }
+        });
     }
 }
